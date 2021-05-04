5da20663221d1b290b7f4d091431f190ff42850c
Tema5volt <82622168+Tema5volt@users.noreply.github.com>
2021-05-05 01:38:57 +0900

    Сохранение №2 [5.05.2021]
    Сделал 2ую страницу сайта

--- Хакатон 2/script.js
+++ Хакатон 2/script.js
@@ -2,14 +2,23 @@ var a=0
 mark1.onclick = function(){
 	if(a==0){
 	fl.style.height = 160+"%";
-	mark1.style.webkitTransform =" rotate (180deg)"
+	this.style.webkitTransform = "rotate(180deg)"
 	a=1;
 	}else
 	if(a==1){
 	fl.style.height = 0+"%";
+	this.style.webkitTransform = "rotate(0deg)"
 	a=0;
 	}
 }
+back1.onclick = function(){
+	alert("впвыарпв")
+	all.style.left = 100+"%";
+}
+left1.onclick = function(){
+	all.style.left = 100+"%";
+}
+
 // fn = function(){
 	// var a=document.getElementById("id").value
 	// console.log(a)
